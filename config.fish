5c23cb8e256619e0ad66239da0e3abce797c8e5c
Maximiliano Céspedes <maxicespedes7@hotmail.com>
2019-10-08 10:35:30 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,2 +1,22 @@
 # ~/.config/fish/config.fish
 alias pl='git pull upstream master --rebase --autostash'
+
+function cb -d "git: Create a new updated branch from master"
+  git checkout master
+  pl 
+  git checkout -b "$argv"
+  echo -n \a ## Lekker man!
+end
+
+function cbs -d "git: Create a new updated branch from master and keep your changes"
+  git stash
+  git checkout master
+  pl 
+  git checkout -b "$argv"
+  git stash pop  
+  echo -n \a ## Lekker man!
+end
+
+function clean_branches -d "git: Clean all your branches not merged to master"
+  git branch | grep -v '^*' | xargs git branch -d
+end
